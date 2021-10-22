0ec70f1abfb295d897b4ec84f5bb0f79190bcdb7
sunny <sunny@sunnymix.com>
2021-10-23 00:49:20 +0800

    完成题目：二叉树的最大深度

--- t2021/src/main/java/week03/tree/MaximumDepthOfBinaryTree.java
+++ t2021/src/main/java/week03/tree/MaximumDepthOfBinaryTree.java
@@ -1,7 +1,17 @@
 package week03.tree;
 
+import common.model.TreeNode;
+
 /**
  * leetcode: https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
  */
 public class MaximumDepthOfBinaryTree {
+    public static void main(String[] args) {
+        // TODO
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
 }
