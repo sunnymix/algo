137d99217fe2cb0847f792d5bf34fdacd0cae483
sunny <sunny@sunnymix.com>
2021-10-23 01:08:29 +0800

    完成题目：二叉树的最小深度

--- t2021/src/main/java/week03/tree/MinimumDepthOfBinaryTree.java
+++ t2021/src/main/java/week03/tree/MinimumDepthOfBinaryTree.java
@@ -1,7 +1,23 @@
 package week03.tree;
 
+import common.model.TreeNode;
+
 /**
  * leetcode: https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/
  */
 public class MinimumDepthOfBinaryTree {
+    public static void main(String[] args) {
+        // TODO
+    }
+
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftMin = minDepth(root.left);
+        int rightMin = minDepth(root.right);
+        return (leftMin == 0 || rightMin == 0) ?
+                leftMin + rightMin + 1 :
+                Math.min(leftMin, rightMin);
+    }
 }
