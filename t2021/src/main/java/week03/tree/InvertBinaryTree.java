2e8ac5ecd37b60d0c2874e8521cb66295d36fda8
sunny <sunny@sunnymix.com>
2021-10-23 00:04:28 +0800

    完成题目：翻转二叉树

--- t2021/src/main/java/week03/tree/InvertBinaryTree.java
+++ t2021/src/main/java/week03/tree/InvertBinaryTree.java
@@ -4,4 +4,38 @@ package week03.tree;
  * leetcode: https://leetcode-cn.com/problems/invert-binary-tree/description/
  */
 public class InvertBinaryTree {
+    public static void main(String[] args) {
+        // TODO: 打印树形结构
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
 }
